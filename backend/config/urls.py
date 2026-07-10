4c0f0824d803826513dba017a802b7e0e3e2b041
Salifouattara <ouattarasalif2005@gmail.com>
2026-07-10 10:27:04 +0000

    Fix: Remove static media configurations to allow Cloudinary absolute URLs

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,6 +6,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
